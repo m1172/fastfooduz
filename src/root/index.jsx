d7a158c356bf7db84142b3d05800e1bdd1374421
m1172 <muhshuh2000@gmail.com>
2021-09-22 22:58:35 +0200

    edited

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import './style';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
+import { Container } from './style';
 
 export const Root = () => {
-  return <h1>root</h1>;
+  return <Container>root</Container>;
 };
 export default Root;
