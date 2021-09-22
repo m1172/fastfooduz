b79d9c0d44dac1ad740cc07fff6da5db54d79b2e
m1172 <muhshuh2000@gmail.com>
2021-09-22 23:11:01 +0200

    edited

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: 100vh;
+  border: 1px solid red;
+  padding: 30px 0;
+`;
