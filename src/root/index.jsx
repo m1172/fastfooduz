7327ba76c8ed19f04e78b1baf016045c142ab970
m1172 <muhshuh2000@gmail.com>
2021-09-27 22:35:53 +0200

    edited

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import { sidebar } from '../utils/sidebar';
 import { Body, Container } from './style';
