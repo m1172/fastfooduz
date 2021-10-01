428bce0b3eae9c3d4226f26f64b6a09d2072eafc
m1172 <muhshuh2000@gmail.com>
2021-10-01 09:34:28 +0200

    edited

--- src/components/Buyurtmalar/style.js
+++ src/components/Buyurtmalar/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 
 export const Header = styled.div`
@@ -13,5 +14,7 @@ export const Header = styled.div`
 export const Wrapper = styled.div`
   display: flex;
   width: ${({ center }) => (center ? '100%' : '300px')};
-  height: 80px;
+  height: 90px;
+  align-items: center;
+  border: 1px solid #e5e5e5;
 `;
