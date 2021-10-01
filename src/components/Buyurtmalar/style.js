01d0129c14a89bca4fb86ab020509dae40ed2725
m1172 <muhshuh2000@gmail.com>
2021-10-01 07:38:07 +0200

    edited

--- src/components/Buyurtmalar/style.js
+++ src/components/Buyurtmalar/style.js
@@ -2,4 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  background: white;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
 `;
