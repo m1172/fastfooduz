3b4bc0b15b8841a4ff86713c11d21c6b82864402
m1172 <muhshuh2000@gmail.com>
2021-10-04 13:28:10 +0200

    edited

---
+++ src/components/Buyurtmalar/Body/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Container } from './style';
+
+export const Body = () => {
+  return <Container>Body</Container>;
+};
+export default Body;
--- src/components/Buyurtmalar/index.jsx
+++ src/components/Buyurtmalar/index.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
+import Body from './Body';
 import { Container } from './style';
 
 export const Buyurtmalar = () => {
   return (
     <Container>
       <Navbar />
+      <Body />
     </Container>
   );
 };
