6efcca4fb3dbfd61d63caa3a2d80b71ecdfc15d8
m1172 <muhshuh2000@gmail.com>
2021-10-07 10:01:43 +0200

    edited

---
+++ src/components/Buyurtmalar/MenuH/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  margin: 35px;
+  overflow: scroll;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 300px;
+  margin-right: 15px;
+  flex: 1;
+`;
