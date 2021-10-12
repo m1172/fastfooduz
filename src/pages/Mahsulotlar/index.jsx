e74169522948e83ec6781c7cf76d9be05429a109
m1172 <muhshuh2000@gmail.com>
2021-10-12 13:12:12 +0200

    mahsulotlar

--- src/pages/Mahsulotlar/index.jsx
+++ src/pages/Mahsulotlar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Generic = ({ location }) => {
+export const Mahsulotlar = ({ location }) => {
   return (
     <div>
       <h1>Coming soon {location.pathname}</h1>
