2aaaf4e7f3d615bd6c1702cdef78bfbf8b8334be
m1172 <muhshuh2000@gmail.com>
2021-10-12 15:05:57 +0200

    edited

--- src/components/Mahsulotlar/index.jsx
+++ src/components/Mahsulotlar/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import { Container } from './style';
 
-export const Mahsulotlar = () => (
-  <Container>
-    <h1>Rest Api</h1>
-  </Container>
-);
+export const Mahsulotlar = () => {
+    const [data, setData] = useState([])
+
+    fetch('robots.txt').then((res) => )
+  return (
+    <Container>
+      <h1>Rest Api</h1>
+    </Container>
+  );
+};
 
 export default Mahsulotlar;
