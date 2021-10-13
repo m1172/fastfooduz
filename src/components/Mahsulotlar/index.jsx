0361cf8c16d5c08a8db766c64343f25331164dc9
m1172 <muhshuh2000@gmail.com>
2021-10-13 21:51:35 +0200

    edited

--- src/components/Mahsulotlar/index.jsx
+++ src/components/Mahsulotlar/index.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react/cjs/react.development';
 import { Container } from './style';
 
 export const Mahsulotlar = () => {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    fetch('robots.txt').then((res) => )
+  fetch('robots.txt').then((res) => res.text());
   return (
     <Container>
       <h1>Rest Api</h1>
